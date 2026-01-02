238204d3d3efb818d245f8b5d6007708184ba8a0
Ritesh <riteshsingh4075rk@gmail.com>
2026-01-02 21:32:14 +0530

    Added how to create a hashmap

---
+++ javacollections/src/map/hashmap/HashMap_Create.java
@@ -0,0 +1,11 @@
+package map.hashmap;
+import java.util.Map;
+import java.util.HashMap;
+public class HashMap_Create {
+    public static void main(String[] args) {
+        // Create a empty hashmap
+        Map<Integer,Integer> map = new HashMap<>();
+         
+        System.out.println(map);
+    }
+}
